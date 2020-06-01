b328e0ac9be01c501d423911da5ef20d60f6851c
Damien Gagnepain <dg.raspoutine@gmail.com>
2020-06-01 16:47:09 +0200

    ex

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,9 @@
+class GamesController < ApplicationController
+  def new
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    @letters = alphabet.sample(10)
+  end
+
+  def score
+  end
+end
